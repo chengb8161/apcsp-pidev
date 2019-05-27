51af4676dfb43f020583220ebc377810a4333b89
chengb8161 <50209668+chengb8161@users.noreply.github.com>
2019-05-27 08:39:07 -0700

    Update student.c

--- cdemo/student.c
+++ cdemo/student.c
@@ -21,6 +21,7 @@ int main()
 {
   struct Student JohnSmith;
   struct Student JaneDoe;
+  char input, name;
 
   strcpy(&JohnSmith.firstName, "John");
   strcpy(&JohnSmith.lastName, "Smith");
@@ -33,7 +34,7 @@ int main()
   JaneDoe.studentID = 011235;
   
   printf("Which student(s)?\n");
-  fgets(input, 256, stdin);
-  sscanf(input, "%s", name);
-  printStudent(struct Student* name)
+  fgets(&input, 256, stdin);
+  sscanf(&input, "%c", &name);
+  printStudent(struct Student* name);
 }
